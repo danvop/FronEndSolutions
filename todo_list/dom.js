2718a4073771a9621705a7e008d73044abdb52c3
danvop <danvop@gmail.com>
2018-08-22 13:23:02 +1000

    append item by button click

--- todo_list/dom.js
+++ todo_list/dom.js
@@ -2,3 +2,17 @@
 // console.log(document.domain);
 // console.log(document.url);
 // console.log(document.title);
+
+var button = document.querySelector("button");
+button.addEventListener("click", function(){
+    var itemList = document.getElementById("items");
+    var li = document.createElement("li");
+    var input = document.querySelector("input");
+    if (input.value !== "") {
+        li.className = "list-group-item";
+        li.textContent = input.value;
+        itemList.appendChild(li);
+    }
+    
+        
+});
