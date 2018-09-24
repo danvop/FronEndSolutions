01d0a9a6ba0546a3fb4e1544f230ce79a2c4da7c
danvop <danvop@gmail.com>
2018-09-24 23:19:11 +0400

    add custom style

--- products_cart/products_cart.js
+++ products_cart/products_cart.js
@@ -6,10 +6,12 @@
 //     [3,"butter",30]
 // ];
 
-var items = [
-    {"id":1, "name":"milk", "price":10},
-    {"id":2, "name":"bread", "price":20},
-    {"id":3, "name":"butter", "price":30},
+var shopItems = [
+    {"id":1, "name":"Milk", "price":10,
+     "img": "https://via.placeholder.com/200x200//000/?text=MILK",
+     "thmb_img": "https://via.placeholder.com/50x50//000?text=M" },
+    {"id":2, "name":"Bread", "price":20},
+    {"id":3, "name":"Butter", "price":30},
 ];
 
 
@@ -18,20 +20,25 @@ var items = [
 var Products = {    
     render : function(){
         var prodList = document.getElementById("products");
-        items.forEach(function(item){
+        shopItems.forEach(function(item){
+            //add text content
             var li = document.createElement("li");
-            var btn = document.createElement("button");
+            li.className+="list-item";
+            li.textContent = item.name + " " + item.price + "$ ";
             
+            //add button "+"
+            var btn = document.createElement("button");
             btn.textContent = "+";
             btn.setAttribute("prod-id", item.id);
             btn.addEventListener("click", (e) => { 
-                var itemId = e.target.getAttribute("prod-id")
-                console.log(itemId);
+                var itemId = Number(e.target.getAttribute("prod-id"));
+                // console.log(itemId);
                 Cart.addItem(itemId);
                 });
-            li.textContent = item.id + " " + item.name + " " + item.price + "$";
             li.appendChild(btn);
+            //append list item to products list
             prodList.appendChild(li);
+            
         });
         //console.log(this.items);
 
@@ -42,14 +49,60 @@ var Products = {
 var Cart = {
     items: [],
     addItem : function(id) {
-        //if Cart.items includes id = id => qty++ else add new item
-        this.items.push(id);
-        console.log(this.items);
         
+        //search item by id in Cart.items
+        var found = this.items.find(item => item.id === id);
+        //if Cart.items doesn't include item
+        if(!found) {
+            //add to Cart.items
+            this.items.push({
+                "id": id,
+                "qty": 1
+            });
+        } else {
+            //increase item quantity
+            found.qty++;
+        }        
+        // console.log(this.items);
+        this.render();
+
+        //store Cart.items in localStorage
+        localStorage.setItem('cartItems', JSON.stringify(this.items));
+
+    },
+    remItem : function(id) {
+
     },
     //rerender cart after add/remove item
     render : function(){
-        
+        var cartList = document.getElementById("cart");
+        var cartSum = 0;
+        cartList.innerHTML = "";
+        this.items.forEach(function(cartItem){
+            var found = shopItems.find(item => item.id === cartItem.id);
+            
+            
+            
+            var li = document.createElement("li");
+            li.textContent = found.name + " " + 
+                             found.price + 
+                             "$" + " " + 
+                             "qty: " + cartItem.qty;
+                             ;
+            li.className+="list-item";
+            cartList.appendChild(li);
+            //calculate total
+            cartSum+= found.price * cartItem.qty;
+        });
+        //render total 
+        var total = document.createElement("p");
+        total.innerHTML = "<b>Total:</b> " + cartSum + "$";
+        cartList.appendChild(total);
+    },
+    reset : function(){
+        document.getElementById("cart").innerHTML = "";
+        Cart.items = [];
+        localStorage.removeItem('cartItems');
     }
 
 }
@@ -58,5 +111,9 @@ var Cart = {
 //HELPERS
 
 Products.render();
+var storedItems = localStorage.getItem('cartItems');
+if(storedItems) {
+    Cart.items = JSON.parse(storedItems);
+    Cart.render();
+}
 
-//Cart.populate();
